05f8e2cbf4eef37b53a1c33f7c1c69714e31f9da
gaeun <mangoade100g@naver.com>
2025-02-04 22:48:17 +0900

    프로젝트 초기 세팅

---
+++ src/main/java/com/example/rechick/ReChickApplication.java
@@ -0,0 +1,13 @@
+package com.example.rechick;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReChickApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ReChickApplication.class, args);
+    }
+
+}
